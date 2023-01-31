2f4fc126c12ddb5dbef8cf6e93075ff009884931
Gador Cruz <gadorcruz@hotmail.com>
2023-01-31 13:56:22 +0000

    starting JS

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,23 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(book_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def delete
+  end
+
+  private
+  def book_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
